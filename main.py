369f8012e55a174369c2bfea6c7b3f558ea96d74
ria24122002 <77931839+ria24122002@users.noreply.github.com>
2023-09-12 13:21:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-
 def count_batteries_by_health(present_capacities):
-  rated_capacity=120
-  soh=[]
-  for i in range(present_capacities):
-    soh%=100*i/rated_capacity
-    soh.append(soh%)
-  health_counts= {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
-  for  i in range(soh):
-    if soh[i]>=80:
-      health_counts["healthy"]+=1
-    elif soh[i]>63 and soh[i]<80:
-       health_counts["exchange"]+=1
+  h=e=f=0
+  for i in present_capacities:
+    a=(100*i)/120
+    if a>=80:
+      h=h+1
+    elif (a>63 and a<80):
+      e=e+1
     else:
-      health_counts["failed"]+=1
+      f=f+1
+  # rated_capacity=120
+  # soh=[]
+  # for i in range(present_capacities):
+  #   soh%=100*i/rated_capacity
+  #   soh.append(soh%)
+  # health_counts= {
+  #   "healthy": 0,
+  #   "exchange": 0,
+  #   "failed": 0
+  # }
+  # for  i in range(soh):
+  #   if soh[i]>=80:
+  #     health_counts["healthy"]+=1
+  #   elif soh[i]>63 and soh[i]<80:
+  #      health_counts["exchange"]+=1
+  #   else:
+  #     health_counts["failed"]+=1
   
     
     
-  return health_counts
+  return  {
+    "healthy": h,
+    "exchange": e,
+    "failed": f
+  }
 
 
 def test_bucketing_by_health():
